6c3f011d95be57e88b9ffb8616eb3409326ad990
wongandrewp <wong.andrew.p@gmail.com>
2017-05-19 12:21:24 -0400

    Most basic functionality

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,5 +1,73 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by andrewwong on 5/19/17.
  */
-public class MyArrayList {
+public class MyArrayList<E> {
+    E[] myArray;
+    private int size = 0;
+
+    MyArrayList(){
+        this.myArray = (E[]) new Object[0];
+    }
+
+    MyArrayList(int capacity){
+        this.myArray = (E[]) new Object[capacity];
+    }
+
+    public boolean add(E element){
+        myArray = Arrays.copyOf(myArray, myArray.length+1);
+
+        myArray[size] = element;
+        size++;
+        if(myArray.length >= size) return true;
+        else return false;
+    }
+
+    public void add(int index, E element){
+        myArray = Arrays.copyOf(myArray, myArray.length+1);
+        myArray[index] = element;
+    }
+
+    public E get(int index){
+        return myArray[index];
+    }
+
+    //TODO resize arraylist after removing element
+    public E remove(int index){
+        E storedElement = (E) myArray[index];
+//        myArray = Arrays.copyOf(myArray, myArray.length-1);
+        return storedElement;
+    }
+
+    public E set(int index, E element){
+        E storedElement = myArray[index];
+        myArray[index] = element;
+        return storedElement;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear(){
+        for(int i = 0; i < size(); i++){
+            myArray[i] = null;
+        }
+        size = 0;
+
+    }
+
+    public boolean isEmpty(){
+        if(size == 0) return true;
+        else return false;
+    }
+
+    public boolean contains(Object o){
+        for(E e: myArray){
+            if (e.equals(o)) return true;
+        }
+        return false;
+    }
 }
--- src/test/java/TestMyArrayList.java
+++ src/test/java/TestMyArrayList.java
@@ -1,5 +1,129 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by andrewwong on 5/19/17.
  */
 public class TestMyArrayList {
+    MyArrayList<Integer> myEmptyIntegerArrayList;
+    MyArrayList<Integer> myNotEmptyIntegerArrayList;
+    Integer integer0 = new Integer(0);
+    Integer integer1 = new Integer(1);
+
+    @Before
+    public void initializeMyArrayLists(){
+        myEmptyIntegerArrayList = new MyArrayList<Integer>();
+        myNotEmptyIntegerArrayList = new MyArrayList<Integer>(2);
+        myNotEmptyIntegerArrayList.add(integer0);
+        myNotEmptyIntegerArrayList.add(integer1);
+
+    }
+    @Test
+    public void testSimpleAdd(){
+        //Given
+        Integer num = new Integer(3);
+        boolean expected = true;
+        //When
+        boolean actual = myEmptyIntegerArrayList.add(num);
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testIndexAdd(){
+        //Given
+        int index = 2;
+        Integer element = new Integer(5);
+        Integer expected = element;
+        //When
+        myNotEmptyIntegerArrayList.add(index, element);
+        Integer actual = myNotEmptyIntegerArrayList.get(index);
+        //Then
+        assertEquals(expected,actual);
+
+    }
+    @Test
+    public void testGet(){
+        //Given
+        Integer expected = new Integer(0);
+        myEmptyIntegerArrayList.add(expected);
+        //When
+        Integer actual = myEmptyIntegerArrayList.get(0);
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testRemove(){
+        //Given
+        Integer expected = integer0;
+        //When
+        Integer actual = myNotEmptyIntegerArrayList.remove(0);
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testSet(){
+        //Given
+        Integer expected = integer1;
+        //When
+        Integer actual = myNotEmptyIntegerArrayList.set(1, new Integer(10));
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testClear(){
+        //Given
+        //When
+        myNotEmptyIntegerArrayList.clear();
+
+        //Then
+        assertEquals(0, myNotEmptyIntegerArrayList.size());
+        assertEquals(null, myNotEmptyIntegerArrayList.get(0));
+    }
+    @Test
+    public void testSize(){
+        //Given
+        int expected = 2;
+        //When
+        int actual = myNotEmptyIntegerArrayList.size();
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testIsNotEmpty(){
+        //Given
+        boolean expected = false;
+        //When
+        boolean actual = myNotEmptyIntegerArrayList.isEmpty();
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testIsEmpty(){
+        //Given
+        boolean expected = true;
+        //When
+        boolean actual = myEmptyIntegerArrayList.isEmpty();
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testContainsTrue(){
+        //Given
+        boolean expected = true;
+        //When
+        boolean actual = myNotEmptyIntegerArrayList.contains(integer0);
+        //Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testContainsFalse(){
+        //Given
+        boolean expected = false;
+        //When
+        boolean actual = myEmptyIntegerArrayList.contains(integer0);
+        //Then
+        assertEquals(expected, actual);
+    }
 }
